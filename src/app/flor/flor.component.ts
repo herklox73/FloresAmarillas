f0da9e03df30a1a80b26d9ad553463a5380df30b
herklox73 <carlosgofin13@gmail.com>
2025-03-21 03:18:46 -0500

    flores

--- src/app/flor/flor.component.ts
+++ src/app/flor/flor.component.ts
@@ -16,7 +16,6 @@ export class FlorComponent {
   isEnvelopeOpen: boolean = false;
   isModalOpen: boolean = false;
   @ViewChild('audioPlayer') audioPlayer!: ElementRef<HTMLAudioElement>;
-  @ViewChild('videoPlayer') videoPlayer!: ElementRef<HTMLVideoElement>;
 
   toggleEnvelope() {
     if (!this.isEnvelopeOpen) {
@@ -24,7 +23,6 @@ export class FlorComponent {
       setTimeout(() => {
         this.isModalOpen = true;
         this.audioPlayer.nativeElement.play();
-        this.videoPlayer.nativeElement.play(); // Reproducir el video al abrir el modal
       }, 500);
     }
   }
@@ -34,7 +32,5 @@ export class FlorComponent {
     this.isEnvelopeOpen = false;
     this.audioPlayer.nativeElement.pause();
     this.audioPlayer.nativeElement.currentTime = 0;
-    this.videoPlayer.nativeElement.pause(); // Pausar el video al cerrar el modal
-    this.videoPlayer.nativeElement.currentTime = 0; 
   }
 }
